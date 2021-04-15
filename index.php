90ab1656c5ef82099befd97230ee13f72cbd86ee
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-04-15 10:49:55 +0200

    ajout du SASS

--- index.php
+++ index.php
@@ -5,22 +5,69 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Jeremy DEJOUX</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+    <link rel="stylesheet" href="ressources/SASS/style.css">
 </head>
 
 <body>
-    <section class="section">
-        <div class="container">
-            <h1 class="title">
-                Jeremy DEJOUX
-            </h1>
-            <p class="subtitle">
-                Dev Web en apprentissage à <strong>La Plateforme_</strong>!
-            </p>
+    <header>
+        <nav>
+            <ul>
+                <li> <a href="#about_me"> About Me </a> </li>
+                <li> <a href="#skills"> Skills </a> </li>
+                <li> <a href="#works"> Work </a> </li>
+                <li> <a href="#contact"> Contact </a> </li>
+            </ul>
+        </nav>
+    </header>
 
-            <button class="login">Button</button>
-        </div>
-    </section>
+
+    <main>
+        <section>
+
+            <h2 id="about_me">About me</h2>
+
+            <article>
+                <p>Je suis Jeremy DEJOUX j'ai 21 ans, je suis actuellement en première année de coding School à la <a href="https://laplateforme.io">Plateforme</a> à Marseille.
+                    Je suis à la recherche d'une alternance pour septembre 2021 en tant que développeur web.
+                </p>
+            </article>
+            
+        </section>
+
+
+        <section>
+            <h2 id="skills">Skills</h2>
+
+                <article>
+                    <img src="ressources/Img/html-css.png" alt="">
+                    <img src="ressources/Img/js.webp" alt="">
+                    <img src="ressources/Img/php.png" alt="">
+                    <img src="ressources/Img/sql.png" alt="">
+                </article>
+        </section>
+
+
+        <section>
+            <h2 id="works">Work</h2>
+        </section>
+        
+
+        <section>
+            <h2 id="contact">Contact</h2>
+        </section>
+    </main>
+
+
+    <footer>
+        <nav>
+            <ul>
+                <li> <a href="#about_me"> About Me </a> </li>
+                <li> <a href="#skills"> Skills </a> </li>
+                <li> <a href="#works"> Work </a> </li>
+                <li> <a href="#contact"> Contact </a> </li>
+            </ul>
+        </nav>
+    </footer>
 </body>
 
 </html>
