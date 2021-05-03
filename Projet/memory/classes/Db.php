711f95cbdb244b9ff27b262ef1778d69857298b7
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-05-03 14:17:26 +0200

    maj db memory

--- Projet/memory/classes/Db.php
+++ Projet/memory/classes/Db.php
@@ -6,8 +6,8 @@ require_once('Wall_Of_Fame.php');
 $_SESSION['user'] = new User;
 $wall = new Wall_of_Fame;
 
-$dsn = "mysql:host=localhost;dbname=memory";
-$userDB = 'root';
-$passDB = '';
+$dsn = "mysql:host=localhost;dbname=jeremy-dejoux_memory";
+$userDB = 'Jeremy';
+$passDB = 'Minato6510';
 $bdd = new PDO("$dsn", "$userDB", "$passDB");
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
